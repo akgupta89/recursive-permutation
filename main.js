9a33792305600cd419920382f85726a1485f86b5
akgupta89 <40367955+akgupta89@users.noreply.github.com>
2018-12-15 10:56:49 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+function findPermutations(originalWord, remainingWord, permutatedWord) {
+		let permutationTotal = [];
+
+		if (originalWord.length == permutatedWord.length) {
+			return [ permutatedWord.join() ];
+		}
+
+		let cloneWord = remainingWord.slice(),
+			charToAdd = cloneWord.shift();
+
+		for (let index = 0; index <= permutatedWord.length; index++) {
+			let permutation = permutatedWord.slice();
+			permutation.splice(index, 0, charToAdd);
+
+			let permutatedArray = findPermutations(originalWord, cloneWord, permutation);
+			permutationTotal = permutationTotal.concat(permutatedArray);
+		}
+
+		return permutationTotal;
+	}
+
+	let word = "TomRiddle";
+
+	let totalPermutations = findPermutations(word, word.split(""), []);
+	console.log(totalPermutations);
+
+	let uniquePermutations = [...new Set(totalPermutations)];
+	console.log(uniquePermutations);
